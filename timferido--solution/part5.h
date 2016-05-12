a1bc77a37dcc070597825c4c579ee4143870681a
timferido <timferido@csu.fullerton.edu>
2016-05-11 22:33:48 -0700

    Create part5.h

---
+++ timferido--solution/part5.h
@@ -0,0 +1,19 @@
+//
+//  part5.h
+//  Assignment03
+//
+//  Created by Timothy Ferido on 3/12/16.
+//  Copyright © 2016 Timothy Ferido. All rights reserved.
+//
+
+#ifndef part5_h
+#define part5_h
+
+int tri_iter(int n);
+
+int pow_iter(int a, int b);
+
+int fib_iter(int n);
+
+
+#endif /* part5_h */
