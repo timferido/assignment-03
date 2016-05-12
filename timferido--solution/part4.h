1a014cfa72b5c5645a8df37a4ea5fec06b05d024
timferido <timferido@csu.fullerton.edu>
2016-05-11 22:33:11 -0700

    Create part4.h

---
+++ timferido--solution/part4.h
@@ -0,0 +1,27 @@
+//
+//  part4.h
+//  Assignment03
+//
+//  Created by Timothy Ferido on 3/12/16.
+//  Copyright © 2016 Timothy Ferido. All rights reserved.
+//
+
+#ifndef part4_h
+#define part4_h
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int fib(int n);
+
+void exception_handler(int n);
+
+void fib_test();
+
+int pow(int a, int b);
+
+int tri(int n);
+
+
+#endif /* part4_h */
