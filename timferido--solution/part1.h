752c6c87d1bc2c8556f75581022708ae0fdc4455
timferido <timferido@csu.fullerton.edu>
2016-05-11 22:30:18 -0700

    Create part1.h

---
+++ timferido--solution/part1.h
@@ -0,0 +1,23 @@
+//
+//  part1.h
+//  Assignment03
+//
+//  Created by Timothy Ferido on 3/12/16.
+//  Copyright © 2016 Timothy Ferido. All rights reserved.
+//
+
+#ifndef part1_h
+#define part1_h
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+/*------------------------------------------------------------------*/
+
+double rad(double a);
+
+void test_exceptions();
+
+
+#endif /* part1_h */
