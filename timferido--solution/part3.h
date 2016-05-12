9de8c4d276a7f97f08560d84d6e3a1dc7e4f389b
timferido <timferido@csu.fullerton.edu>
2016-05-11 22:32:21 -0700

    Create part3.h

---
+++ timferido--solution/part3.h
@@ -0,0 +1,26 @@
+//
+//  part3.h
+//  Assignment03
+//
+//  Created by Timothy Ferido on 3/12/16.
+//  Copyright © 2016 Timothy Ferido. All rights reserved.
+//
+
+#ifndef part3_h
+#define part3_h
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <list>
+#include <set>
+
+void list_example();
+
+void set_example();
+
+void vector_example();
+
+void part3_test();
+
+#endif /* part3_h */
