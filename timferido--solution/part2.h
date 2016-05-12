ce90d74c499a2990fb635204e3386a383b11b242
timferido <timferido@csu.fullerton.edu>
2016-05-11 22:31:32 -0700

    Create part2.h

---
+++ timferido--solution/part2.h
@@ -0,0 +1,21 @@
+//
+//  part2.h
+//  Assignment03
+//
+//  Created by Timothy Ferido on 3/12/16.
+//  Copyright © 2016 Timothy Ferido. All rights reserved.
+//
+
+#ifndef part2_h
+#define part2_h
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+template <class T>
+T Large(T n1, T n2);
+int part2();
+
+#endif /* part2_h */
